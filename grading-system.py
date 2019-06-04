9144e9fddda425147c859d1eb0f60c95a785fefe
John Matson <john@matsonengineering.com>
2019-06-03 20:01:09 -0700

    v2.0 working error checking

--- grading-system.py
+++ grading-system.py
@@ -12,34 +12,43 @@ class Student:
 
 class Course:
     student_list = []
-    Student = Student()
+    student = Student()
 
     def add_student(self):
-        self.Student.student_number = input('Student Number: ')
-        self.Student.lab_grade = float(input('Lab Grade: '))
-        self.Student.quiz_grade = float(input('Quiz Grade: '))
-        self.Student.midterm_grade = float(input('Midterm Grade: '))
-        self.Student.final_exam_grade = float(input('Final Exam Grade: '))
-        self.Student.final_grade = (0.4 * self.Student.lab_grade + 0.1 * self.Student.quiz_grade + 0.2 * self.Student.midterm_grade + 0.3 * self.Student.final_exam_grade)
-        self.student_list.append(self.Student)
+        print('Student Number: ', end='')
+        self.student.student_number = self.id_error_check()
+        print('Lab Grade: ', end='')
+        self.student.lab_grade = self.grade_error_check()
+        print('Quiz Grade: ', end='')
+        self.student.quiz_grade = self.grade_error_check()
+        print('Midterm Grade: ', end='')
+        self.student.midterm_grade = self.grade_error_check()
+        print('Final Exam Grade: ', end='')
+        self.student.final_exam_grade = self.grade_error_check()
+        self.student.final_grade = (0.4 * self.student.lab_grade + 0.1 * self.student.quiz_grade + 0.2 * self.student.midterm_grade + 0.3 * self.student.final_exam_grade)
+        self.student_list.append(self.student)
 
     def edit_student(self):
         student = input('Which student would you like to edit: ')
-        self.Student.student_number = input('Student Number: ')
-        self.Student.lab_grade = float(input('Lab Grade: '))
-        self.Student.quiz_grade = float(input('Quiz Grade: '))
-        self.Student.midterm_grade = float(input('Midterm Grade: '))
-        self.Student.final_exam_grade = float(input('Final Exam Grade: '))
-        self.Student.final_grade = (0.4 * self.Student.lab_grade + 0.1 * self.Student.quiz_grade + 0.2 * self.Student.midterm_grade + 0.3 * self.Student.final_exam_grade)
+        print('Student Number: ', end='')
+        self.student.student_number = self.id_error_check()
+        print('Lab Grade: ', end='')
+        self.student.lab_grade = self.grade_error_check()
+        print('Quiz Grade: ', end='')
+        self.student.quiz_grade = self.grade_error_check()
+        print('Midterm Grade: ', end='')
+        self.student.midterm_grade = self.grade_error_check()
+        print('Final Exam Grade: ', end='')
+        self.student.final_exam_grade = self.grade_error_check()
+        self.student.final_grade = (0.4 * self.student.lab_grade + 0.1 * self.student.quiz_grade + 0.2 * self.student.midterm_grade + 0.3 * self.student.final_exam_grade)
         del self.student_list[int(student) - 1]
-        self.student_list.insert((int(student) - 1), self.Student)
+        self.student_list.insert((int(student) - 1), self.student)
 
     def delete_student(self):
         student = input('Which student would you like to delete: ')
-        self.student_list.remove(int(student) - 1)
+        del self.student_list[int(student) - 1]
 
     def print_grades(self):
-        # print(self.student_list[0].student_number)
         for i in range(len(self.student_list)):
             print(self.student_list[i].student_number)
             print(self.student_list[i].lab_grade)
@@ -53,6 +62,36 @@ class Course:
 
     def save_class(self):
         pass
+
+
+    def id_error_check(self):
+        while True:
+            student_id = input()
+            if len(student_id) != 9:
+                print('Error, please try again: ', end='')
+            elif (student_id[0] != 'a') and (student_id[0] != 'A'):
+                print('Error, please try again: ', end='')
+            elif student_id[1] != '0':
+                print('Error, please try again: ', end='')
+            elif (student_id[2] != '0') and (student_id[2] != '1'):
+                print('Error, please try again: ', end='')
+            else:
+                return student_id
+
+    def grade_error_check(self):
+        while True:
+            string_id = input()
+            for i in range(len(string_id)):
+                if (string_id[i] != '.') and not (string_id[i].isdigit()):
+                    print('Error, please try again: ', end='')
+                    break
+                else:
+                    float_id = float(string_id)
+                    if(float_id < 0) or (float_id > 100):
+                        print('Error, please try again: ', end='')
+                        break
+                    else:
+                        return float_id
 
 
 def print_menu():
@@ -69,24 +108,24 @@ def print_menu():
     print("CMD>", end="")
 
 
-Course = Course()
+course = Course()
 exit_loop = False
 while True:
     print_menu()
     cmd_input = input()
 
     if(cmd_input == 'a'):
-        Course.add_student()
+        course.add_student()
     elif(cmd_input == 'e'):
-        Course.edit_student()
+        course.edit_student()
     elif(cmd_input == 'd'):
-        Course.delete_student()
+        course.delete_student()
     elif(cmd_input == 'p'):
-        Course.print_grades()
+        course.print_grades()
     elif(cmd_input == 'l'):
-        Course.load_class()
+        course.load_class()
     elif(cmd_input == 's'):
-        Course.save_student()
+        course.save_class()
     elif(cmd_input == 'q'):
         exit_loop = True
     else:
